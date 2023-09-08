a39e6393aaeb654db7baccaa39325ea63c7e58d3
TheCalculus <thecalculus@users.noreply.github.com>
2023-09-08 21:44:43 +1000

    .

--- csrc/server.new.c
+++ csrc/server.new.c
@@ -9,11 +9,13 @@
 #include <string.h>
 #include <sys/event.h>
 #include <sys/types.h>
-#include <unistd.h>
+#include <time.h>
+#include <unistd.h>            // assume defined(__unix__)
 
 #include "sse_helper.h"
 
-#define PORT   8080
+#define SSE_SERVER_PORT   8080
+#define SSE_INTERVAL_SECS 1
 
 struct kevent  event , tevent;
 int            COUNT , server_fd , client_fds[MAX_CLIENTS],
@@ -32,7 +34,8 @@ volatile int   RUNNING = 1;
     }
 
 
-static void handle_sigint(int signum) { 
+static void
+handle_sigint(int signum) { 
     printf("SIGINT\n");
     RUNNING = 0;
 }
@@ -65,23 +68,25 @@ send_sse(int fd) {
 }
 
 int server() {
+//  dispatch_queue_t 
+
     signal(SIGINT, handle_sigint);
     signal(SIGSTOP, handle_sigint);
 
     struct sockaddr_in server_addr , client_addr;
 
-    /*    sse_helper.h, initialised line 19 
+    /*    defined sse_helper.h, initialised line 19 
      *    extern struct kevent  event , tevent;
      *    extern int            COUNT , server_fd , client_fds[MAX_CLIENTS],
      *                          kq    , result    , client_count;
      */
 
     EXITIFTRU((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1);
-    memset   (&server_addr, 0, sizeof(server_addr));
+    memset(&server_addr, 0, sizeof(server_addr));
 
     server_addr.sin_family      = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port        = htons(PORT);
+    server_addr.sin_port        = htons(SSE_SERVER_PORT);
 
     EXITIFTRU(bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1);
     EXITIFTRU(listen(server_fd, 5) == -1);
@@ -96,7 +101,9 @@ int server() {
 
         for (int i = 0; i < result; i++) {
             int fd = (int)((&tevent)[i].ident);
+
             send_sse(fd);
+            usleep(500000);
 
             if ((&tevent)[i].flags & EV_EOF) {
                 printf("%d disconnected\n", fd);
@@ -132,12 +139,13 @@ int server() {
                 // this write has to happen upon successful read
                 printf("%ld bytes read\n", request_len);
 
-                const char* response = "HTTP/1.1 200 OK\r\n"
-                                       "Content-Type: text/event-stream\r\n"
-                                       "Cache-Control: no-cache\r\n"
-                                       "Connection: keep-alive\r\n"
-                                       "Data: \"beginning_sse\"\r\n"
-                                       "Access-Control-Allow-Origin: *\r\n\r\n";
+                const char* response =
+                    "HTTP/1.1 200 OK\r\n"
+                    "Content-Type: text/event-stream\r\n"
+                    "Cache-Control: no-cache\r\n"
+                    "Connection: keep-alive\r\n"
+                    "Data: \"beginning_sse\"\r\n"
+                    "Access-Control-Allow-Origin: *\r\n\r\n";
 
                 write(fd, response, strlen(response));
             }
@@ -148,8 +156,7 @@ int server() {
                 snprintf(response, sizeof(response),
                         "event: JS_ELEMENT_RUN\ndata: { \"target\": \"#target\","
                         "\"positions\": [0, 3],"
-                        "\"callfront\": %d }\n\n",
-                        COUNT++);
+                        "\"callfront\": %d }\n\n", COUNT);
 
                 ssize_t bytes_written = write(fd, response, strlen(response));
                 EXITIFTRU(bytes_written == -1);
